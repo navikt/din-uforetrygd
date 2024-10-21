0253adc69b17bfbf0cd7e202075469425301eee6
Bjørn Gilstad <bjorn.gilstad@nav.no>
2024-10-21 14:41:17 +0200

    Legg til styling og ta i bruk komponenter

--- min-uforetrygd-frontend/src/app/layout.tsx
+++ min-uforetrygd-frontend/src/app/layout.tsx
@@ -1,40 +1,58 @@
-import "@navikt/ds-css"
-import { Heading } from "@navikt/ds-react"
-import { fetchDecoratorReact } from "@navikt/nav-dekoratoren-moduler/ssr"
-import Script from "next/script"
+import "@navikt/ds-css";
+import { Heading } from "@navikt/ds-react";
+import { fetchDecoratorReact } from "@navikt/nav-dekoratoren-moduler/ssr";
+import Script from "next/script";
 import getEnv from "@/utils/env";
+import "./layout.css";
 
-const RootLayout = async ({ children }: Readonly<{ children: React.ReactNode }>) => {
-    const decoratorEnv = (getEnv("DECORATOR_ENV") ?? "prod") as "dev" | "prod"
-    const mode = getEnv("MODE") as "borger" | "veileder"
+const RootLayout = async ({
+  children,
+}: Readonly<{ children: React.ReactNode }>) => {
+  const decoratorEnv = (getEnv("DECORATOR_ENV") ?? "prod") as "dev" | "prod";
+  const mode = getEnv("MODE") as "borger" | "veileder";
 
-    const Decorator = await fetchDecoratorReact({
-        env: decoratorEnv
-    })
-    if (mode === "veileder") {
-        return (
-            <html lang="no">
-                <body>
-                    <Heading size="xlarge" level="1">{process.env.MODE}</Heading>
-                    {children}
-                </body>
-            </html>
-        )
-    }
-
+  const Decorator = await fetchDecoratorReact({
+    env: decoratorEnv,
+    params: {
+      context: "privatperson",
+      breadcrumbs: [
+        {
+          title: "Min side",
+          url: "https://www.nav.no",
+        },
+        {
+          title: "Uføretrygd",
+          url: "https://www.nav.no/uføretrygd",
+        },
+      ],
+    },
+  });
+  if (mode === "veileder") {
     return (
-        <html lang="no">
-            <head>
-                <Decorator.HeadAssets/>
-            </head>
-            <body>
-                <Decorator.Header/>
-                    {children}
-                <Decorator.Footer/>
-                <Decorator.Scripts loader={Script}/>
-            </body>
-        </html>
-    )
-}
+      <html lang="no">
+        <body>
+          <Heading size="xlarge" level="1">
+            {process.env.MODE}
+          </Heading>
+          {children}
+        </body>
+      </html>
+    );
+  }
 
-export default RootLayout
+  return (
+    <html lang="no">
+      <head>
+        <Decorator.HeadAssets />
+      </head>
+      <body>
+        <Decorator.Header />
+        <main className="main-content">{children}</main>
+        <Decorator.Footer />
+        <Decorator.Scripts loader={Script} />
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
--- min-uforetrygd-frontend/src/app/page.tsx
+++ min-uforetrygd-frontend/src/app/page.tsx
@@ -1,10 +1,97 @@
-import { Box } from "@navikt/ds-react/Box";
-import { Heading } from "@navikt/ds-react";
+import { Box, Heading, Link } from "@navikt/ds-react";
+import { Kort, KortGrid } from "./components/Kort";
+import {
+  EnvelopeClosedIcon,
+  FileTextIcon,
+  SackKronerIcon,
+} from "@navikt/aksel-icons";
+import { SakTilBehandling } from "./components/SakTilBehandling";
 
 export default function Home() {
   return (
-      <Box>
-        <Heading size="xlarge" level="1">Uføretrygd</Heading>
-      </Box>
+    <>
+      <Heading size="xlarge" level="1">
+        Uføretrygd
+      </Heading>
+
+      <SakTilBehandling />
+
+      <section>
+        <Heading level="2" size="medium">
+          Dine saker til behandling
+        </Heading>
+      </section>
+
+      <section>
+        <Heading level="2" size="medium">
+          Informasjon og registreringer
+        </Heading>
+        <KortGrid>
+          <Kort
+            title="Utbetalinger"
+            description="Oversikt og detaljer"
+            link="about:blank"
+            icon={<SackKronerIcon />}
+          />
+          <Kort
+            title="Brev for uføretrygd"
+            description="Vedtak med mer"
+            link="about:blank"
+            icon={<EnvelopeClosedIcon />}
+          />
+          <Kort
+            title="Sakene dine"
+            description="Status på søknader og vedtak"
+            link="about:blank"
+            icon={<FileTextIcon />}
+          />
+          <Kort
+            title="Famileforhold"
+            description="Samboerforhold, sivilstand, barn"
+            link="about:blank"
+            icon={<FileTextIcon />}
+          />
+          <Kort
+            title="Dine fullmakter"
+            description="Gi fullmakt og se dine fullmakter"
+            link="about:blank"
+            icon={<FileTextIcon />}
+          />
+          <Kort
+            title="Ettersend informasjon"
+            description="Til uføresaken din"
+            link="about:blank"
+            icon={<FileTextIcon />}
+          />
+        </KortGrid>
+      </section>
+
+      <section>
+        <Box
+          background="surface-alt-3-subtle"
+          padding="4"
+          borderRadius="xlarge"
+        >
+          <Heading level="2" size="medium">
+            Husk å gi oss beskjed om endringer i din situasjon
+          </Heading>
+          <Link href="#">
+            Se hvile endringer du må si fra om og hvordan du sier fra.
+          </Link>
+        </Box>
+      </section>
+
+      <section>
+        <Heading level="2" size="medium">
+          Relevante søknader
+        </Heading>
+      </section>
+
+      <section>
+        <Heading level="2" size="medium">
+          Kan være aktuelt for deg
+        </Heading>
+      </section>
+    </>
   );
 }
