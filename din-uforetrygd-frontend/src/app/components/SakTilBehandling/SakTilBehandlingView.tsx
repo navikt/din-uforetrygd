48a7f57fe8b4f00db7fb096543b1485b390a1d62
Bjørn Gilstad <bjorn.gilstad@nav.no>
2024-10-21 14:41:02 +0200

    Legg til sak til behandling komponent

---
+++ din-uforetrygd-frontend/src/app/components/SakTilBehandling/SakTilBehandlingView.tsx
@@ -0,0 +1,22 @@
+import { GuidePanel, Heading, Link, List } from "@navikt/ds-react";
+import { ListItem } from "@navikt/ds-react/List";
+
+export const SakTilBehandlingView = () => {
+  return (
+    <section>
+      <GuidePanel>
+        <Heading level="2" size="medium">
+          Søknaden din om uføretrygd er under behandling
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="#">Se saksbehandlingstider for uføretrygd</Link>
+          </ListItem>
+          <ListItem>
+            <Link href="#">Se saken din</Link>
+          </ListItem>
+        </List>
+      </GuidePanel>
+    </section>
+  );
+};
