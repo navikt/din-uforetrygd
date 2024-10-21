a30bfed81ba03d598f5223d34082c95da542679b
Bjørn Gilstad <bjorn.gilstad@nav.no>
2024-10-21 16:52:28 +0200

    Legg til divided linklist variant

--- min-uforetrygd-frontend/src/app/components/LinkList/LinkList.tsx
+++ min-uforetrygd-frontend/src/app/components/LinkList/LinkList.tsx
@@ -1,12 +1,23 @@
+import clsx from "clsx";
 import styles from "./linklist.module.css";
 
 interface ILinkListProps {
   children: React.ReactNode[];
+  variant?: "simple" | "divided";
 }
 export const LinkList: React.FC<ILinkListProps> = (props) => (
-  <ul className={styles.linkList}>
+  <ul
+    className={clsx(styles.linkList, {
+      [styles.dividedVariant]: props.variant === "divided",
+    })}
+  >
     {props.children.map((child) => (
-      <li key={child?.toString()} className={styles.link}>
+      <li
+        key={child?.toString()}
+        className={clsx(styles.link, {
+          [styles.dividedVariantLink]: props.variant === "divided",
+        })}
+      >
         {child}
       </li>
     ))}
