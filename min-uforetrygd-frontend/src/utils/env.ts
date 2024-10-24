ebc610a7001af560a2bdf514816d2222a427db80
olelomo-nav <ole.peder.lomo@nav.no>
2024-10-24 14:41:54 +0200

    Strukturerer Relavante søknader med visningslogikk
    Co-authored-by: Bjørn Gilstad <bjorn.gilstad@nav.no>

--- min-uforetrygd-frontend/src/utils/env.ts
+++ min-uforetrygd-frontend/src/utils/env.ts
@@ -1,6 +1,11 @@
 import { unstable_noStore as noStore} from "next/cache";
 
-type Env = "DECORATOR_ENV" | "MODE"
+type Env = "DECORATOR_ENV"
+    | "MODE"
+    | "UFORETRYGD_BACKEND"
+    | "LINK_SOKNAD_GRADERT_UFORE"
+    | "LINK_SOKNAD_UFORE"
+    | "LINK_SOKNAD_BARNETILLEGG"
 
 export default function getEnv(env: Env ) {
     noStore()
---
+++ min-uforetrygd-frontend/src/utils/filterShowFor/filterShowFor.test.ts
@@ -0,0 +1,39 @@
+import {describe, expect, it} from "vitest";
+import {Visningskriterier} from "@/const";
+import filterShowFor from "./index";
+
+
+const lenker = [
+    {
+        href: "#",
+        text: "Søknad om endret inntektsgrense - gradert uføretrygd",
+        showFor: [Visningskriterier.GradertUfore, Visningskriterier.Uforetrygd]
+    },
+    {
+        href: "#",
+        text: "Søknad om uføretrygd",
+        showFor: []
+    },
+    {
+        href: "#",
+        text: "Søknad om barnetillegg til uføretrygd",
+        showFor: []
+    }
+]
+
+describe("filterShowFor", () => {
+    it("should show relevante lenker when no ufore", () => {
+        const actual = filterShowFor([], lenker).map(lenke => lenke.text)
+        expect(actual).toEqual(["Søknad om uføretrygd", "Søknad om barnetillegg til uføretrygd"])
+    })
+
+    it("should show relevante lenker when gradert ufore", () => {
+        const actual = filterShowFor([Visningskriterier.GradertUfore, Visningskriterier.Uforetrygd], lenker).map(lenke => lenke.text)
+        expect(actual).toEqual([
+            "Søknad om endret inntektsgrense - gradert uføretrygd",
+            "Søknad om uføretrygd",
+            "Søknad om barnetillegg til uføretrygd"
+        ])
+    })
+
+})
