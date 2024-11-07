9d9b399f1a818a3b3f5585c06c18c6e9b637810f
olelomo-nav <ole.peder.lomo@nav.no>
2024-11-07 13:28:46 +0100

    Håndterer veileder token utveksling
    Co-authored-by: Bjørn Gilstad <bjorn.gilstad@nav.no>

--- din-uforetrygd-frontend/src/api/getOboToken.ts
+++ din-uforetrygd-frontend/src/api/getOboToken.ts
@@ -1,11 +1,14 @@
 import { headers } from "next/headers";
 import {
   getToken,
+  requestAzureOboToken,
   requestTokenxOboToken,
+  validateAzureToken,
   validateIdportenToken,
 } from "@navikt/oasis";
+import getEnv from "@/utils/env";
 
-const getOboToken = async () => {
+const getOboTokenTokenX = async () => {
   return new Promise(async (resolve, reject) => {
     if (process.env.NODE_ENV !== "production") {
       resolve("mock-token");
@@ -21,7 +24,7 @@ const getOboToken = async () => {
     }
     const obo = await requestTokenxOboToken(
       token,
-      "dev-gcp:pensjonselvbetjening:uforetrygd-backend", // TODO: Bruk miljøvariabel
+      getEnv("BACKEND_DIN_UFORETRYGD_SCOPE")!,
     );
 
     if (!obo.ok) {
@@ -31,4 +34,32 @@ const getOboToken = async () => {
     resolve(obo.token);
   });
 };
-export default getOboToken;
+
+const getOboTokenAzure = async () => {
+  return new Promise(async (resolve, reject) => {
+    if (process.env.NODE_ENV !== "production") {
+      resolve("mock-token");
+    }
+    const clientHeaders = await headers();
+    const token = getToken(clientHeaders);
+    if (!token) {
+      return reject("Missing wonderwall cookie");
+    }
+    const validation = await validateAzureToken(token);
+    if (!validation.ok) {
+      return reject(`Validation failed: ${validation.error}`);
+    }
+    const obo = await requestAzureOboToken(
+        token,
+        getEnv("BACKEND_DIN_UFORETRYGD_SCOPE")!,
+    );
+
+    if (!obo.ok) {
+      return reject(`OBO Exchange failed: ${obo.error}`);
+    }
+
+    resolve(obo.token);
+  });
+};
+
+export default getEnv("MODE") === 'veileder' ? getOboTokenAzure : getOboTokenTokenX
