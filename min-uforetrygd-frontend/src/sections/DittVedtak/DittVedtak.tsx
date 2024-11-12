55a7532ebe8d670d00feb5f558105183fe5dc874
olelomo-nav <ole.peder.lomo@nav.no>
2024-11-12 10:36:01 +0100

    Lenker for dev-miljø

--- min-uforetrygd-frontend/src/sections/DittVedtak/DittVedtak.tsx
+++ min-uforetrygd-frontend/src/sections/DittVedtak/DittVedtak.tsx
@@ -1,38 +1,27 @@
-import { Visningskriterier } from "@/const";
-import { ShowMore } from "@/components/ShowMore";
-import { BodyShort, Heading, Link, List } from "@navikt/ds-react";
-import { ListItem } from "@navikt/ds-react/List";
-import { dittUforevedtak } from "@/api/endpoints";
-import { format, parseISO } from "date-fns";
+import { Visningskriterier } from '@/const'
+import { ShowMore } from '@/components/ShowMore'
+import { BodyShort, Heading, Link, List } from '@navikt/ds-react'
+import { ListItem } from '@navikt/ds-react/List'
+import { dittUforevedtak } from '@/api/endpoints'
+import { format, parseISO } from 'date-fns'
+import getEnv from '@/utils/env'
 
 interface IDittVedtak {
-  visningskriterier: Visningskriterier[];
-  pid?: string;
+  visningskriterier: Visningskriterier[]
+  pid?: string
 }
 
-export const DittVedtak: React.FC<IDittVedtak> = async ({
-  visningskriterier,
-  pid,
-}) => {
+export const DittVedtak: React.FC<IDittVedtak> = async ({ visningskriterier, pid }) => {
   if (visningskriterier.includes(Visningskriterier.Uforetrygd)) {
-    const dittUforevedtakData = await dittUforevedtak(pid);
-    const uforegrad = dittUforevedtakData?.uforegrad ?? 0;
-    const uforetidspunkt = format(
-      parseISO(dittUforevedtakData?.uforetidspunkt ?? ""),
-      "dd.MM.yyyy",
-    );
-    const uforetrygdInnvilget = format(
-      parseISO(dittUforevedtakData?.virkFom ?? ""),
-      "dd.MM.yyyy",
-    );
-    const hasVarigTilrettelagtArbeid =
-      dittUforevedtakData?.hasVarigTilrettelagtArbeid ?? false;
-    const hasBarnetilleggFellesBarn =
-      dittUforevedtakData?.hasBarnetilleggFellesBarn ?? false;
-    const hasBarnetilleggSaerkullsbarn =
-      dittUforevedtakData?.hasBarnetilleggSaerkullsbarn ?? false;
-    const hasGjenlevendeTillegg =
-      dittUforevedtakData?.hasGjenlevendeTillegg ?? false;
+    const linkInntektsplanlegger = getEnv('LINK_INNTEKTSPLANLEGGER')
+    const dittUforevedtakData = await dittUforevedtak(pid)
+    const uforegrad = dittUforevedtakData?.uforegrad ?? 0
+    const uforetidspunkt = format(parseISO(dittUforevedtakData?.uforetidspunkt ?? ''), 'dd.MM.yyyy')
+    const uforetrygdInnvilget = format(parseISO(dittUforevedtakData?.virkFom ?? ''), 'dd.MM.yyyy')
+    const hasVarigTilrettelagtArbeid = dittUforevedtakData?.hasVarigTilrettelagtArbeid ?? false
+    const hasBarnetilleggFellesBarn = dittUforevedtakData?.hasBarnetilleggFellesBarn ?? false
+    const hasBarnetilleggSaerkullsbarn = dittUforevedtakData?.hasBarnetilleggSaerkullsbarn ?? false
+    const hasGjenlevendeTillegg = dittUforevedtakData?.hasGjenlevendeTillegg ?? false
 
     return (
       <section>
@@ -50,20 +39,14 @@ export const DittVedtak: React.FC<IDittVedtak> = async ({
             <ListItem>Uføregrad {uforegrad} prosent</ListItem>
             <ListItem>Uføretidspunkt {uforetidspunkt}</ListItem>
             <ListItem>Uføretrygd ble invilget {uforetrygdInnvilget}</ListItem>
-            {hasVarigTilrettelagtArbeid && (
-              <ListItem>Du har tiltaket Varig tilrettelagt arbeid</ListItem>
-            )}
+            {hasVarigTilrettelagtArbeid && <ListItem>Du har tiltaket Varig tilrettelagt arbeid</ListItem>}
           </List>
           <Heading level="3" size="small">
             Tillegg til uføretrygden
           </Heading>
           <List>
-            {hasBarnetilleggFellesBarn && (
-              <ListItem>Barnetillegg for fellesbarn</ListItem>
-            )}
-            {hasBarnetilleggSaerkullsbarn && (
-              <ListItem>Barnetillegg for særkullsbarn</ListItem>
-            )}
+            {hasBarnetilleggFellesBarn && <ListItem>Barnetillegg for fellesbarn</ListItem>}
+            {hasBarnetilleggSaerkullsbarn && <ListItem>Barnetillegg for særkullsbarn</ListItem>}
             {hasGjenlevendeTillegg && <ListItem>Gjenlevendetillegg</ListItem>}
           </List>
 
@@ -71,14 +54,12 @@ export const DittVedtak: React.FC<IDittVedtak> = async ({
             Inntektsgrense og registrert forventet inntekt
           </Heading>
           <BodyShort>
-            Har du inntekt ved siden av uføretrygden? Du finner dine
-            inntektsgrenser, trekkprosent (kompensasjonsgrad) og hvilken inntekt
-            vi har beregnet din uføretrygd ut fra, på førstesiden i{" "}
-            <Link href="#">inntektsplanleggeren</Link>.
+            Har du inntekt ved siden av uføretrygden? Du finner dine inntektsgrenser, trekkprosent (kompensasjonsgrad)
+            og hvilken inntekt vi har beregnet din uføretrygd ut fra, på førstesiden i{' '}
+            <Link href={linkInntektsplanlegger}>inntektsplanleggeren</Link>.
           </BodyShort>
         </ShowMore>
       </section>
-    );
+    )
   }
-};
-
+}
--- min-uforetrygd-frontend/src/sections/MeldeFra/MeldeFra.tsx
+++ min-uforetrygd-frontend/src/sections/MeldeFra/MeldeFra.tsx
@@ -1,27 +1,23 @@
-import {Visningskriterier} from "@/const";
-import { Box, Link, Heading } from "@navikt/ds-react";
+import { Visningskriterier } from '@/const'
+import { Box, Link, Heading } from '@navikt/ds-react'
+import getEnv from '@/utils/env'
 
 interface IMeldeFra {
-    visningskriterier: Visningskriterier[];
+  visningskriterier: Visningskriterier[]
 }
 
 export const MeldeFra: React.FC<IMeldeFra> = ({ visningskriterier }) => {
-    if (visningskriterier.includes(Visningskriterier.Uforetrygd)) {
-        return (
-            <section>
-                <Box
-                    background="surface-alt-3-subtle"
-                    padding="4"
-                    borderRadius="xlarge"
-                >
-                    <Heading level="2" size="medium">
-                        Husk å gi oss beskjed om endringer i din situasjon
-                    </Heading>
-                    <Link href="#">
-                        Se hvile endringer du må si fra om og hvordan du sier fra.
-                    </Link>
-                </Box>
-            </section>
-        )
-    }
+  if (visningskriterier.includes(Visningskriterier.Uforetrygd)) {
+    const link = getEnv('LINK_MELD_FRA_OM_ENDRINGER')
+    return (
+      <section>
+        <Box background="surface-alt-3-subtle" padding="4" borderRadius="xlarge">
+          <Heading level="2" size="medium">
+            Husk å gi oss beskjed om endringer i din situasjon
+          </Heading>
+          <Link href={link}>Se hvile endringer du må si fra om og hvordan du sier fra.</Link>
+        </Box>
+      </section>
+    )
+  }
 }
