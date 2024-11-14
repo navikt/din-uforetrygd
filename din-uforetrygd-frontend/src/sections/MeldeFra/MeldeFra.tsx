ca46a8fa7039d178b81f3face931b5aa15729131
olelomo-nav <ole.peder.lomo@nav.no>
2024-11-14 12:20:12 +0100

    Styler Meldefra
    Co-authored-by: Bjørn Gilstad <bjorn.gilstad@nav.no>

--- din-uforetrygd-frontend/src/sections/MeldeFra/MeldeFra.tsx
+++ din-uforetrygd-frontend/src/sections/MeldeFra/MeldeFra.tsx
@@ -1,6 +1,7 @@
 import { Visningskriterier } from '@/const'
-import { Box, Link, Heading } from '@navikt/ds-react'
+import { Box, Link, Heading, VStack } from '@navikt/ds-react'
 import getEnv from '@/utils/env'
+import styles from './MeldeFra.module.css'
 
 interface IMeldeFra {
   visningskriterier: Visningskriterier[]
@@ -10,12 +11,14 @@ export const MeldeFra: React.FC<IMeldeFra> = ({ visningskriterier }) => {
   if (visningskriterier.includes(Visningskriterier.Uforetrygd)) {
     const link = getEnv('LINK_MELD_FRA_OM_ENDRINGER')
     return (
-      <section>
-        <Box background="surface-action-subtle" padding="4" borderRadius="xlarge">
-          <Heading level="2" size="medium">
-            Husk å gi oss beskjed om endringer i din situasjon
-          </Heading>
-          <Link href={link}>Se hvilke endringer du må si fra om og hvordan du sier fra.</Link>
+      <section className={styles.meldeFra}>
+        <Box background="surface-action-subtle" paddingBlock="8" paddingInline="6" borderRadius="xlarge">
+          <VStack gap="2">
+            <Heading level="2" size="medium">
+              Husk å gi oss beskjed om endringer i din situasjon
+            </Heading>
+            <Link href={link}>Se hvilke endringer du må si fra om og hvordan du sier fra.</Link>
+          </VStack>
         </Box>
       </section>
     )
