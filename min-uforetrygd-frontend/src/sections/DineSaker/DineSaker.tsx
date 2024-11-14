1406b8584fedad37d77a981b3f11862a53f12fcf
olelomo-nav <ole.peder.lomo@nav.no>
2024-11-14 13:34:05 +0100

    Lenker til din uforetrygdsak

--- min-uforetrygd-frontend/src/sections/DineSaker/DineSaker.tsx
+++ min-uforetrygd-frontend/src/sections/DineSaker/DineSaker.tsx
@@ -1,23 +1,24 @@
-import {LinkList} from "@/components/LinkList";
-import { Heading, Link } from "@navikt/ds-react";
-import {Visningskriterier} from "@/const";
+import { LinkList } from '@/components/LinkList'
+import { Heading, Link } from '@navikt/ds-react'
+import { Visningskriterier } from '@/const'
+import { getUrl } from '@/utils/getUrl'
 
 interface IDineSaker {
-    visningskriterier: Visningskriterier[];
+  visningskriterier: Visningskriterier[]
 }
 
-export const DineSaker: React.FC<IDineSaker> = ( {visningskriterier} ) => {
-
-    if (visningskriterier.includes(Visningskriterier.UforesoknadTilBehandling)) {
-        return (
-            <section>
-                <Heading level="2" size="medium">
-                    Dine saker til behandling
-                </Heading>
-                <LinkList>
-                    <Link href="#">Uføretrygd</Link>
-                </LinkList>
-            </section>
-        )
-    }
+export const DineSaker: React.FC<IDineSaker> = async ({ visningskriterier }) => {
+  if (visningskriterier.includes(Visningskriterier.UforesoknadTilBehandling)) {
+    const saksoversiktLenke = await getUrl('LINK_SAKER')
+    return (
+      <section>
+        <Heading level="2" size="medium">
+          Dine saker til behandling
+        </Heading>
+        <LinkList>
+          <Link href={saksoversiktLenke}>Uføretrygd</Link>
+        </LinkList>
+      </section>
+    )
+  }
 }
