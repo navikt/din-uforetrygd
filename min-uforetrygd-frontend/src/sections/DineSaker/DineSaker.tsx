9a7de1cfd09b8c26e28b3a57214abb0fadcb7642
olelomo-nav <ole.peder.lomo@nav.no>
2024-11-19 12:47:08 +0100

    Fikser feil som ble oppdaget av test

--- min-uforetrygd-frontend/src/sections/DineSaker/DineSaker.tsx
+++ min-uforetrygd-frontend/src/sections/DineSaker/DineSaker.tsx
@@ -5,11 +5,12 @@ import { getUrl } from '@/utils/getUrl'
 
 interface IDineSaker {
   visningskriterier: Visningskriterier[]
+  pid: string | undefined
 }
 
-export const DineSaker: React.FC<IDineSaker> = async ({ visningskriterier }) => {
+export const DineSaker: React.FC<IDineSaker> = async ({ visningskriterier, pid }) => {
   if (visningskriterier.includes(Visningskriterier.UforesoknadTilBehandling)) {
-    const saksoversiktLenke = await getUrl('LINK_SAKER')
+    const saksoversiktLenke = await getUrl('LINK_SAKER', pid)
     return (
       <section>
         <Heading level="2" size="medium">
