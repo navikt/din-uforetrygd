7b08979bfb034342f8f804274d42f38c05f6af73
Bjørn Gilstad <bjorn.gilstad@nav.no>
2024-11-20 12:10:29 +0100

    Fix responsiveness for kort

--- din-uforetrygd-frontend/src/components/Kort/KortGrid.tsx
+++ din-uforetrygd-frontend/src/components/Kort/KortGrid.tsx
@@ -1,13 +1,13 @@
-import { HGrid } from "@navikt/ds-react";
+import { HGrid } from '@navikt/ds-react'
 
 interface IKortGridProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 export const KortGrid: React.FC<IKortGridProps> = (props) => {
   return (
-    <HGrid gap="6" columns={2}>
+    <HGrid gap="6" columns={{ md: 2 }}>
       {props.children}
     </HGrid>
-  );
-};
+  )
+}
