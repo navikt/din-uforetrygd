3709a3482f196d115168c586103517cb8b1aec26
Bjørn Gilstad <bjorn.gilstad@nav.no>
2024-11-25 08:28:26 +0100

    Sentrer fullmakt banner

--- min-uforetrygd-frontend/src/components/RepresentasjonBanner/RepresentasjonBanner.tsx
+++ min-uforetrygd-frontend/src/components/RepresentasjonBanner/RepresentasjonBanner.tsx
@@ -1,12 +1,16 @@
 'use client'
+import styles from './RepresentasjonBanner.module.css'
 
 export const RepresentasjonBanner = () => {
   // Selv om komponenten er 'use client' vil next prøve å rendere den på serveren, og da vil window.location.origin være undefined.
-  if (!window.location.origin) return
+  if (!window) return
   return (
-    <representasjon-banner
-      representasjonstyper="PENSJON_FULLSTENDIG,PENSJON_BEGRENSET,UFORETRYGD_LES"
-      redirectTo={`${window.location.origin}/uforetrygd/selvbetjening`}
-    ></representasjon-banner>
+    <div className={styles.representasjonBannerWrapper}>
+      <representasjon-banner
+        className={styles.representasjonBanner}
+        representasjonstyper="PENSJON_FULLSTENDIG,PENSJON_BEGRENSET,UFORETRYGD_LES"
+        redirectTo={`${window.location.origin}/uforetrygd/selvbetjening`}
+      ></representasjon-banner>
+    </div>
   )
 }
