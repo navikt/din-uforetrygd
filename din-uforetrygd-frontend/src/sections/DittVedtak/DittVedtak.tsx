ff2e8cfd832a6f3fb338138305a278ef9593ead5
Bjørn Gilstad <bjorn.gilstad@nav.no>
2024-11-25 10:24:37 +0100

    PEB-881: Fiks bredde paa Ditt Vedtak

--- din-uforetrygd-frontend/src/sections/DittVedtak/DittVedtak.tsx
+++ din-uforetrygd-frontend/src/sections/DittVedtak/DittVedtak.tsx
@@ -29,56 +29,58 @@ export const DittVedtak: React.FC<IDittVedtak> = async ({ visningskriterier, pid
     const currentYear = new Date().getFullYear()
 
     return (
-      <section className={styles.dittVedtak}>
-        <ShowMore
-          heading="Ditt uførevedtak, inntekt og inntektsgrenser"
-          aria-labelledby="info-heading"
-          collapsedHeight="10rem"
-          scrollBackOnCollapse={false}
-          variant="subtle"
-          as="section"
-          headingSize="medium"
-          headingLevel="2"
-        >
-          <List>
-            <ListItem>Uføregrad {uforegrad} prosent</ListItem>
-            <ListItem>Uføretidspunkt {uforetidspunkt}</ListItem>
-            <ListItem>Uføretrygd innvilget {uforetrygdInnvilget}</ListItem>
-            {hasVarigTilrettelagtArbeid && <ListItem>Du har tiltaket Varig tilrettelagt arbeid</ListItem>}
-          </List>
-          <Heading level="3" size="small">
-            Tillegg til uføretrygden
-          </Heading>
-          <List>
-            {hasBarnetilleggFellesBarn && <ListItem>Barnetillegg for fellesbarn</ListItem>}
-            {hasBarnetilleggSaerkullsbarn && <ListItem>Barnetillegg for særkullsbarn</ListItem>}
-            {hasGjenlevendeTillegg && <ListItem>Gjenlevendetillegg</ListItem>}
-          </List>
-          <VStack gap="6">
-            <VStack>
-              <Heading level="3" size="small">
-                Din inntektsgrense: {inntektsgrense}&nbsp;kr
-              </Heading>
-              <BodyShort>
-                Tjener du mer enn dette, vil du få lavere utbetaling av uføretrygd. Vi reduserer uføretrygden din av
-                beløpet du tjener over inntektsgrensen. Beløpet opp til inntektsgrensen blir du aldri trukket for. Bruk{' '}
-                <Link href={linkInntektsplanlegger}>inntektsplanleggeren</Link> for å se hvordan inntekt påvirker
-                utbetalingen av uføretrygden din.
-              </BodyShort>
+      <div className={styles.dittVedtakWrapper}>
+        <section className={styles.dittVedtak}>
+          <ShowMore
+            heading="Ditt uførevedtak, inntekt og inntektsgrenser"
+            aria-labelledby="info-heading"
+            collapsedHeight="10rem"
+            scrollBackOnCollapse={false}
+            variant="subtle"
+            as="section"
+            headingSize="medium"
+            headingLevel="2"
+          >
+            <List>
+              <ListItem>Uføregrad {uforegrad} prosent</ListItem>
+              <ListItem>Uføretidspunkt {uforetidspunkt}</ListItem>
+              <ListItem>Uføretrygd innvilget {uforetrygdInnvilget}</ListItem>
+              {hasVarigTilrettelagtArbeid && <ListItem>Du har tiltaket Varig tilrettelagt arbeid</ListItem>}
+            </List>
+            <Heading level="3" size="small">
+              Tillegg til uføretrygden
+            </Heading>
+            <List>
+              {hasBarnetilleggFellesBarn && <ListItem>Barnetillegg for fellesbarn</ListItem>}
+              {hasBarnetilleggSaerkullsbarn && <ListItem>Barnetillegg for særkullsbarn</ListItem>}
+              {hasGjenlevendeTillegg && <ListItem>Gjenlevendetillegg</ListItem>}
+            </List>
+            <VStack gap="6">
+              <VStack>
+                <Heading level="3" size="small">
+                  Din inntektsgrense: {inntektsgrense}&nbsp;kr
+                </Heading>
+                <BodyShort>
+                  Tjener du mer enn dette, vil du få lavere utbetaling av uføretrygd. Vi reduserer uføretrygden din av
+                  beløpet du tjener over inntektsgrensen. Beløpet opp til inntektsgrensen blir du aldri trukket for.
+                  Bruk <Link href={linkInntektsplanlegger}>inntektsplanleggeren</Link> for å se hvordan inntekt påvirker
+                  utbetalingen av uføretrygden din.
+                </BodyShort>
+              </VStack>
+              <VStack>
+                <Heading level="3" size="small">
+                  Din registrerte inntekt i {currentYear}: {sumAvForventedeInntekter}&nbsp;kr
+                </Heading>
+                <BodyShort>
+                  Forventet inntekt kan komme fra dine tidligere registreringer, eller i noen tilfeller fra opplysninger
+                  vi har hentet. Du kan endre registrert inntekt i{' '}
+                  <Link href={linkInntektsplanlegger}>inntektsplanleggeren</Link>.
+                </BodyShort>
+              </VStack>
             </VStack>
-            <VStack>
-              <Heading level="3" size="small">
-                Din registrerte inntekt i {currentYear}: {sumAvForventedeInntekter}&nbsp;kr
-              </Heading>
-              <BodyShort>
-                Forventet inntekt kan komme fra dine tidligere registreringer, eller i noen tilfeller fra opplysninger
-                vi har hentet. Du kan endre registrert inntekt i{' '}
-                <Link href={linkInntektsplanlegger}>inntektsplanleggeren</Link>.
-              </BodyShort>
-            </VStack>
-          </VStack>
-        </ShowMore>
-      </section>
+          </ShowMore>
+        </section>
+      </div>
     )
   }
 }
