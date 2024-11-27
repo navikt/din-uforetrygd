1ccd3de6bba3cfbb5749187881e2ac3252746af5
Bjørn Gilstad <bjorn.gilstad@nav.no>
2024-11-27 10:24:27 +0100

    Format more logging

--- din-uforetrygd-frontend/src/utils/logger.ts
+++ din-uforetrygd-frontend/src/utils/logger.ts
@@ -1,32 +1,40 @@
 const log = (
   message: string,
   fields: Record<string, string | number>,
-  level: 'info' | 'warning' | 'error' | 'fatal'
+  level: 'Info' | 'Warning' | 'Error' | 'Fatal'
 ): void => {
   const logEntry = {
     '@timestamp': new Date(),
     ...fields,
     message,
-    severity: level,
+    http: {
+      request: {
+        method: 'TEST',
+        status_code: 200,
+        duration: 92,
+        path: '/bla/bla',
+      },
+    },
+    level: level,
   }
   // eslint-disable-next-line no-console
   console.log(JSON.stringify(logEntry))
 }
 
 export const info = (message: string, fields: Record<string, string | number>) => {
-  log(message, fields, 'info')
+  log(message, fields, 'Info')
 }
 
 export const error = (message: string, fields: Record<string, string | number>) => {
-  log(message, fields, 'error')
+  log(message, fields, 'Error')
 }
 
 export const warning = (message: string, fields: Record<string, string | number>) => {
-  log(message, fields, 'warning')
+  log(message, fields, 'Warning')
 }
 
 export const fatal = (message: string, fields: Record<string, string | number>) => {
-  log(message, fields, 'fatal')
+  log(message, fields, 'Fatal')
 }
 
 export default { info, error, warning, fatal }
