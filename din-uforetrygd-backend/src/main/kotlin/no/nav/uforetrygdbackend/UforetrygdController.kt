2df88c9403542d782601ba4c3102f43bc1d79a2d
Anders Bye <anders.bye@nav.no>
2024-12-02 13:53:46 +0100

    PEB-911 ufore - audit log - initial commit

--- din-uforetrygd-backend/src/main/kotlin/no/nav/uforetrygdbackend/UforetrygdController.kt
+++ din-uforetrygd-backend/src/main/kotlin/no/nav/uforetrygdbackend/UforetrygdController.kt
@@ -1,6 +1,8 @@
 package no.nav.uforetrygdbackend
 
 import no.nav.uforetrygdbackend.security.SecurityContextUtil
+import no.nav.uforetrygdbackend.security.TokenService
+import no.nav.uforetrygdbackend.audit.Auditor
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,16 +12,25 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("api")
 class UforetrygdController(
-    private val uforetrygdService: UforetrygdService
+    private val uforetrygdService: UforetrygdService,
+    private val tokenService: TokenService,
+    private val auditor: Auditor
 ) {
 
     @GetMapping("initiate")
     fun initiateUforetrygd(): ResponseEntity<UforetrygdResponse> {
         val pid = SecurityContextUtil.getPidFromContext()
-        return try {
-            ResponseEntity
+        try {
+            val response = ResponseEntity
                 .status(HttpStatus.OK)
                 .body(uforetrygdService.constructUforetrygdResponse(pid))
+
+            if (tokenService.isUserLoggedInAsSaksbehandler()) {
+                auditor.auditInternalUserRead(tokenService.determineLoggedInUserId(), SecurityContextUtil.getPidFromContext())
+            } else if (SecurityContextUtil.isFullmakt()) {
+                auditor.auditFullmaktRead(tokenService.determineLoggedInUserId(), SecurityContextUtil.getPidFromContext())
+            }
+            return response
         } catch (e: Exception) {
             throw ErrorHandler.exceptionToErrorResponse(
                 exception = e,
