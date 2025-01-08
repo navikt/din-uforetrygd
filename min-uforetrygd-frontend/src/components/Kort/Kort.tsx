d87828f28af6f0f2530638b8a07b74449f8b8596
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-01-08 12:41:29 +0100

    Legg til modal for fullmakt og tilpass kort

--- min-uforetrygd-frontend/src/components/Kort/Kort.tsx
+++ min-uforetrygd-frontend/src/components/Kort/Kort.tsx
@@ -1,43 +1,67 @@
+'use client'
 import { BodyShort, Box, Heading, HStack, VStack } from '@navikt/ds-react'
 import { ChevronRightIcon, FileExportIcon } from '@navikt/aksel-icons'
 import styles from './kort.module.css'
-import React, { ForwardRefExoticComponent, RefAttributes, SVGProps } from 'react'
+import React, { ForwardRefExoticComponent, RefAttributes, SVGProps, useRef } from 'react'
+import FullmaktModal from '../FullmaktModal'
+import checkClientFullmakt from '@/utils/checkClientFullmakt'
 
 interface IKortProps {
   title: string
   description?: string
   href: string
   icon?: ForwardRefExoticComponent<SVGProps<SVGSVGElement> & RefAttributes<SVGSVGElement>>
+  showFullmaktWarning?: boolean
 }
 
 export const Kort: React.FC<IKortProps> = (props) => {
-  return (
-    <a href={props.href} className={styles.kort}>
-      <Box background="surface-neutral-subtle" borderRadius="large" padding="5" className={styles.kortBox}>
-        <HStack align="center" justify="space-between" wrap={false}>
-          <HStack gap="6" align="center" wrap={false}>
-            <Box
-              background="surface-neutral-subtle"
-              className={styles.kortIcon}
-              borderRadius="xlarge"
-              padding="3"
-              aria-hidden
-            >
-              {props.icon ? <props.icon fontSize="1.5rem" /> : <FileExportIcon />}
-            </Box>
-            <VStack justify="center" gap="1">
-              <Heading level="3" size="small" style={{ textDecoration: 'none' }}>
-                {props.title}
-              </Heading>
-              {props.description && <BodyShort>{props.description}</BodyShort>}
-            </VStack>
-          </HStack>
+  const modalRef = useRef<HTMLDialogElement>(null)
 
-          <span className={styles.chevronIcon} aria-hidden>
-            <ChevronRightIcon fontSize="1.5rem" />
-          </span>
-        </HStack>
-      </Box>
-    </a>
+  const isFullmakt = React.useMemo(checkClientFullmakt, [])
+
+  const openLink = () => {
+    window.open(props.href, '_self')
+  }
+
+  const handleOnClick = () => {
+    // Open modal if going to page with no fullmakt support
+    if (isFullmakt && props.showFullmaktWarning) {
+      modalRef?.current?.showModal()
+    } else {
+      openLink()
+    }
+  }
+
+  return (
+    <>
+      <a onClick={handleOnClick} href="javascript:void(0)" className={styles.kort}>
+        <Box background="surface-neutral-subtle" borderRadius="large" padding="5" className={styles.kortBox}>
+          <HStack align="center" justify="space-between" wrap={false}>
+            <HStack gap="6" align="center" wrap={false}>
+              <Box
+                background="surface-neutral-subtle"
+                className={styles.kortIcon}
+                borderRadius="xlarge"
+                padding="3"
+                aria-hidden
+              >
+                {props.icon ? <props.icon fontSize="1.5rem" /> : <FileExportIcon />}
+              </Box>
+              <VStack justify="center" gap="1">
+                <Heading level="3" size="small" style={{ textDecoration: 'none' }}>
+                  {props.title}
+                </Heading>
+                {props.description && <BodyShort>{props.description}</BodyShort>}
+              </VStack>
+            </HStack>
+
+            <span className={styles.chevronIcon} aria-hidden>
+              <ChevronRightIcon fontSize="1.5rem" />
+            </span>
+          </HStack>
+        </Box>
+      </a>
+      <FullmaktModal modalRef={modalRef} onAccept={openLink} />
+    </>
   )
 }
