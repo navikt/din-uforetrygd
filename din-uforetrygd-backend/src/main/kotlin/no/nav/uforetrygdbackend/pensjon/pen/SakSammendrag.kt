518352102f4ce4f23ec002a0e7b804f679cc3ff5
Sæbø <gard.sebo@nav.no>
2025-01-31 12:29:24 +0100

    Uføretrygd backend skal ta i bruk nytt endepuntk og

--- din-uforetrygd-backend/src/main/kotlin/no/nav/uforetrygdbackend/pensjon/pen/SakSammendrag.kt
+++ din-uforetrygd-backend/src/main/kotlin/no/nav/uforetrygdbackend/pensjon/pen/SakSammendrag.kt
@@ -6,8 +6,6 @@ import java.time.LocalDate
 data class SakSammendrag(val sakId: Long,
                          val sakType: String,
                          val sakStatus: String,
-                         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss[xxxx]")
                          val fomDato: LocalDate?,
-                         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss[xxxx]")
                          val tomDato: LocalDate?,
                          val enhetId: String)
