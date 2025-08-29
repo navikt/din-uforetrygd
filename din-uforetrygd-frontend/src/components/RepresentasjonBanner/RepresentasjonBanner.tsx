747d08bbdc15e651b43cea3e0801a6512ef773ad
henrikolsvik <henrik-olsvik@outlook.com>
2025-08-29 15:27:38 +0200

    Reverter reprensentasjonstyper

--- din-uforetrygd-frontend/src/components/RepresentasjonBanner/RepresentasjonBanner.tsx
+++ din-uforetrygd-frontend/src/components/RepresentasjonBanner/RepresentasjonBanner.tsx
@@ -7,7 +7,7 @@ export const RepresentasjonBanner = () => {
     <div className={styles.representasjonBannerWrapper}>
       <representasjon-banner
         className={styles.representasjonBanner}
-        representasjonstyper="PENSJON_FULLSTENDIG,PENSJON_BEGRENSET,UFORETRYGD_LES,UFORETRYGD_KOMMUNISER,UFORETRYGD_SKRIV"
+        representasjonstyper="PENSJON_FULLSTENDIG,PENSJON_BEGRENSET,UFORETRYGD_LES"
         redirectTo={`${window.location.origin}/uforetrygd/selvbetjening`}
       ></representasjon-banner>
     </div>
