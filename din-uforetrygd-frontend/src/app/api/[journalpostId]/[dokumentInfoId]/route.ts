37d25cc6b86e5d95f51c9c73f15a906ed1ef6039
henrikolsvik <henrik-olsvik@outlook.com>
2025-09-18 12:11:28 +0200

    Endrer mappestruktur for proxyendepunkt

--- din-uforetrygd-frontend/src/app/api/[journalpostId]/[dokumentInfoId]/route.ts
+++ din-uforetrygd-frontend/src/app/api/[journalpostId]/[dokumentInfoId]/route.ts
@@ -4,10 +4,10 @@ import { redirect } from 'next/navigation'
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: Promise<{ journalpostId: string; dokumentInfoId: string}> }
+  { params }: { params: Promise<{ journalpostId: string; dokumentInfoId: string }> }
 ) {
   const baseUrl = process.env.UFORETRYGD_BACKEND
-  const { journalpostId, dokumentInfoId} = await params
+  const { journalpostId, dokumentInfoId } = await params
   const token = await getOboToken()
   const variantformat = request.nextUrl.searchParams.get('variantformat')
   const pid = request.nextUrl.searchParams.get('pid')
