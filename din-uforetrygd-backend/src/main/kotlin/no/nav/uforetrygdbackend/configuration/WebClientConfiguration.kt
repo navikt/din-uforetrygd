38838f850231151fcac361408a19fbb345af89c8
Anders Bye <anders.bye@nav.no>
2025-10-08 12:48:12 +0200

    PEB-1315 Din uføre: støtte for kryptert pid i url

--- din-uforetrygd-backend/src/main/kotlin/no/nav/uforetrygdbackend/configuration/WebClientConfiguration.kt
+++ din-uforetrygd-backend/src/main/kotlin/no/nav/uforetrygdbackend/configuration/WebClientConfiguration.kt
@@ -1,41 +1,28 @@
 package no.nav.uforetrygdbackend.configuration
 
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 import reactor.netty.http.client.HttpClient
-import reactor.netty.transport.ProxyProvider
-import java.net.URI
 
 @Configuration
 class WebClientConfiguration {
-
-    @Bean("webClientProxy")
-    fun webClientProxy(): WebClient = (System.getenv("HTTP_PROXY")
-        ?.let { URI(it) }
-        ?.run {
-            WebClient.builder()
-                .clientConnector(
-                    ReactorClientHttpConnector(
-                        HttpClient.create()
-                            .proxy {
-                                it
-                                    .type(ProxyProvider.Proxy.HTTP)
-                                    .host(host)
-                                    .port(port)
-                            }
-                    )
-                )
-                .build()
-        }
-        ?: WebClient.builder().build())
+    private val logger = LoggerFactory.getLogger(WebClientConfiguration::class.java)
 
     @Bean
     fun webClient(): WebClient = WebClient.builder()
         .clientConnector(ReactorClientHttpConnector(HttpClient.create()))
         .exchangeStrategies(ExchangeStrategies.builder().codecs { it.defaultCodecs().maxInMemorySize(16 * 1024 * 1024) }.build())
+        .filter(logRequest())
         .build()
 
+    private fun logRequest(): ExchangeFilterFunction = ExchangeFilterFunction.ofRequestProcessor { clientRequest ->
+        logger.info("Request: ${clientRequest.method()} ${clientRequest.url()}")
+        Mono.just(clientRequest)
+    }
 }
