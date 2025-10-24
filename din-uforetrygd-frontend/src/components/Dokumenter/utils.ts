a0b7269dcf9d1ae67583a8bced914b4ce9088c56
eirikstadheim <eirik.stadheim@nav.no>
2025-10-24 11:03:50 +0200

    Revert endringer i path da det må samsvare med nais config

--- din-uforetrygd-frontend/src/components/Dokumenter/utils.ts
+++ din-uforetrygd-frontend/src/components/Dokumenter/utils.ts
@@ -4,7 +4,7 @@ export const getDocumentProxyLink = (
   variantformat?: string,
   pid?: string
 ) => {
-  const proxyLink = `/din-uforetrygd/selvbetjening/api/${journalpostId}/${dokumentInfoId}`
+  const proxyLink = `/uforetrygd/selvbetjening/api/${journalpostId}/${dokumentInfoId}`
   // pid og variantformat er kun i bruk i veilederløsning og brukes alltid i par
   if (pid && variantformat) return `${proxyLink}?variantformat=${variantformat}&pid=${pid}`
   return proxyLink
