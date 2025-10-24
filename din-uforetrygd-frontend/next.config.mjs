eaaab1063631d698a5633428a9a2b42beda4c11e
eirikstadheim <eirik.stadheim@nav.no>
2025-10-24 11:03:50 +0200

    Revert endringer i path da det må samsvare med nais config

--- din-uforetrygd-frontend/next.config.mjs
+++ din-uforetrygd-frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/din-uforetrygd/selvbetjening"
+    basePath: "/uforetrygd/selvbetjening"
 };
 
 export default nextConfig;
