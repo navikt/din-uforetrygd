80da495175dcc30fb5e4aaeb31ff808769324634
eirikstadheim <eirik.stadheim@nav.no>
2025-10-27 14:31:29 +0100

    Utilgjengelig-banner

--- din-uforetrygd-frontend/src/app/page.tsx
+++ din-uforetrygd-frontend/src/app/page.tsx
@@ -38,6 +38,7 @@ const Home: React.FC<IHomeProps> = async ({ searchParams }) => {
         <VeilederBorgerinformasjon pid={uforetrygdResponse.pid} />
         <EventProvider>
           <main className="main-content" id="maincontent" tabIndex={-1}>
+            <Alert variant="info" style={{ width: '992px' }}>Din uføretrygd kan være utilgjengelig mellom klokken 15:00 og 15:30 pga vedlikehold</Alert>
             <Heading size="xlarge" level="1">
               Din uføretrygd
             </Heading>
