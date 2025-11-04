64509f35cbe0b8e3aaef8a5034e8d21610456c9d
Hanne Æsøy Nes <hanne.esoy.nes@nav.no>
2025-11-04 13:48:22 +0100

    UFO-169: Byttet ut egne snarveier med Aksel LinkCard

--- din-uforetrygd-frontend/src/sections/InformasjonOgRegistreringer/InformasjonOgRegistreringer.tsx
+++ din-uforetrygd-frontend/src/sections/InformasjonOgRegistreringer/InformasjonOgRegistreringer.tsx
@@ -1,5 +1,4 @@
-import { Heading, VStack } from '@navikt/ds-react'
-import { Kort, KortGrid } from '@/components/Kort'
+import { BoxNew, Heading, HGrid, Hide, LinkCard, VStack } from '@navikt/ds-react'
 import {
   FileExportIcon,
   FileTextIcon,
@@ -12,6 +11,10 @@ import { Innloggingstype, Visningskriterier } from '@/const'
 import filterShowFor, { matchAll, matchSome } from '@/utils/filterShowFor'
 import { getUrl } from '@/utils/getUrl'
 import { MinIdDokumentModal } from '@/components/MidIdDokumentModal/MinIdDokumentModal'
+import { LinkCardAnchor, LinkCardDescription, LinkCardIcon, LinkCardTitle } from '@navikt/ds-react/LinkCard'
+import React from 'react'
+import { getFullmaktProps } from '@/utils/fullmakt'
+import { showModal } from '@/utils/showMinIdModal'
 
 interface IInformasjonOgRegistreringerProps {
   visningskriterier: Visningskriterier[]
@@ -25,7 +28,7 @@ const getLinks = async (pid: string | undefined, bprofFullmakt: boolean) => [
     href: await getUrl({ urlFromEnv: 'LINK_UTBETALINGER', pid: pid }),
     title: 'Utbetalinger',
     description: 'Oversikt og detaljer',
-    icon: WalletIcon,
+    icon: <WalletIcon fontSize="2rem" />,
     showFor: true,
     showFullmaktWarning: false,
     visInnloggingsModal: false,
@@ -34,7 +37,7 @@ const getLinks = async (pid: string | undefined, bprofFullmakt: boolean) => [
     href: await getUrl({ urlFromEnv: 'LINK_INNTEKTSPLANLEGGER', pid: pid }),
     title: 'Inntektsplanlegger',
     description: 'Meld fra om endring i inntekt',
-    icon: FileTextIcon,
+    icon: <FileTextIcon fontSize="2rem" />,
     showFor: matchAll([Visningskriterier.Uforetrygd]),
     showFullmaktWarning: false,
     visInnloggingsModal: false,
@@ -43,7 +46,7 @@ const getLinks = async (pid: string | undefined, bprofFullmakt: boolean) => [
     href: await getUrl({ urlFromEnv: 'LINK_DOKUMENTOVERSIKT', pid: pid }),
     title: 'Se alle dokumentene dine',
     description: 'Gå til dokumenter',
-    icon: FileTextIcon,
+    icon: <FileTextIcon fontSize="2rem" />,
     showFor: true,
     showFullmaktWarning: false,
     visInnloggingsModal: true,
@@ -52,7 +55,7 @@ const getLinks = async (pid: string | undefined, bprofFullmakt: boolean) => [
     href: await getUrl({ urlFromEnv: 'LINK_SKATTETREKK', pid: pid }),
     title: 'Frivillig skattetrekk',
     description: 'Registrer tilleggstrekk',
-    icon: ReceiptIcon,
+    icon: <ReceiptIcon fontSize="2rem" />,
     showFor: true,
     showFullmaktWarning: false,
     visInnloggingsModal: false,
@@ -61,7 +64,7 @@ const getLinks = async (pid: string | undefined, bprofFullmakt: boolean) => [
     href: await getUrl({ urlFromEnv: 'LINK_FAMILIEFORHOLD', pid: pid }),
     title: 'Familieforhold',
     description: 'Samboerforhold, sivilstand, barn',
-    icon: PersonPlusIcon,
+    icon: <PersonPlusIcon fontSize="2rem" />,
     showFor: true,
     showFullmaktWarning: false,
     visInnloggingsModal: false,
@@ -70,7 +73,7 @@ const getLinks = async (pid: string | undefined, bprofFullmakt: boolean) => [
     href: await getUrl({ urlFromEnv: bprofFullmakt ? 'LINK_BPROF_FULLMAKTER' : 'LINK_FULLMAKTER', pid: pid }),
     title: 'Dine fullmakter',
     description: 'Gi fullmakt og se dine fullmakter',
-    icon: PersonGroupIcon,
+    icon: <PersonGroupIcon fontSize="2rem" />,
     showFor: true,
     showFullmaktWarning: true,
     visInnloggingsModal: false,
@@ -79,7 +82,7 @@ const getLinks = async (pid: string | undefined, bprofFullmakt: boolean) => [
     href: await getUrl({ urlFromEnv: 'LINK_ETTERSENDE', pid: pid }),
     title: 'Ettersend dokumentasjon',
     description: 'Ettersend dokumenter om saken din',
-    icon: FileExportIcon,
+    icon: <FileExportIcon fontSize="2rem" />,
     showFor: matchSome([Visningskriterier.SakTilBehandling, Visningskriterier.Uforetrygd]),
     showFullmaktWarning: true,
     visInnloggingsModal: false,
@@ -95,21 +98,28 @@ export const InformasjonOgRegistreringer: React.FC<IInformasjonOgRegistreringerP
         <Heading level="2" size="medium">
           Snarveier
         </Heading>
-        <KortGrid>
+        <HGrid gap="6" columns={{ md: 2 }}>
           {relevantLinks.map((link) => (
-            <Kort
-              key={link.title}
-              title={link.title}
-              description={link.description}
-              href={link.href ?? ''}
-              icon={link.icon}
-              showFullmaktWarning={link.showFullmaktWarning}
-              visInnloggingsModal={link.visInnloggingsModal}
-              innloggingstype={props.innloggingstype}
-            />
+            <LinkCard key={link.title}>
+              <Hide below="md" asChild>
+                <BoxNew asChild borderRadius="8" padding="space-8" background="neutral-moderateA">
+                  <LinkCardIcon>{link.icon}</LinkCardIcon>
+                </BoxNew>
+              </Hide>
+              <LinkCardTitle>
+                <LinkCardAnchor
+                  href={link.href ?? ''}
+                  {...getFullmaktProps(link.showFullmaktWarning)}
+                  {...showModal(props.innloggingstype, link.visInnloggingsModal)}
+                >
+                  {link.title}
+                </LinkCardAnchor>
+              </LinkCardTitle>
+              <LinkCardDescription>{link.description}</LinkCardDescription>
+            </LinkCard>
           ))}
           <MinIdDokumentModal innloggingstype={props.innloggingstype} />
-        </KortGrid>
+        </HGrid>
       </VStack>
     </section>
   )
