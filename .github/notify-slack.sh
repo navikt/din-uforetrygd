69072c13ea20b8b4a5eefd2e7ebf36e685913e4c
Hanne Æsøy Nes <hanne.esoy.nes@nav.no>
2025-11-14 13:50:22 +0100

    Fjerner debug

--- .github/notify-slack.sh
+++ .github/notify-slack.sh
@@ -10,25 +10,9 @@ function notify() {
     cd ${tempfolder}
     keyfile=$(mktemp)
 
-    echo -e "${PRIVATE_KEY}" > "${keyfile}"
-    chmod 600 "${keyfile}"
+    cat <<< "${PRIVATE_KEY}" > "${keyfile}"
 
-    echo "Debug: Key file created: ${keyfile}"
-    echo "Debug: Key file size: $(wc -c < "${keyfile}") bytes"
-    echo "Debug: Key file permissions: $(ls -la "${keyfile}")"
-    echo "Debug: First line: $(head -n 1 "${keyfile}")"
-    echo "Debug: Last line: $(tail -n 1 "${keyfile}")"
-    echo "Debug: Line count: $(wc -l < "${keyfile}") lines"
-
-    # Test the key format
-    echo "Debug: Testing key format..."
-    ssh-keygen -l -f "${keyfile}" 2>&1 || echo "Debug: Key format validation failed"
-
-    echo "Debug: Attempting git clone..."
-    GIT_SSH_COMMAND="ssh -i ${keyfile} -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -v" git clone git@github.com:navikt/pensjon-github-to-slack-username.git 2>&1
-    clone_status=$?
-    echo "Debug: Clone exit status: ${clone_status}"
-
+    GIT_SSH_COMMAND="ssh -i ${keyfile} -o IdentitiesOnly=yes" git clone git@github.com:navikt/pensjon-github-to-slack-username.git 2>&1
     rm ${keyfile}
     res=$(grep ${USER} pensjon-github-to-slack-username/brukernavnoversikt.csv)
     status=$?
@@ -37,7 +21,6 @@ function notify() {
             IFS="," read -r github slack slack_id team <<< "${res}"
             slack_id="<@$slack_id>"
     else
-            echo "Debug: Bruker '${USER}' ikke funnet i brukernavnoversikt.csv"
             slack_id=${USER}
             team=unknown
     fi
