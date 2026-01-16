b2b156adff50565c6ecf4a72973e8c0d020d94d0
André Mossige <andre.mossige@nav.no>
2026-01-16 11:17:39 +0100

    Sett timeout på Unleash

--- din-uforetrygd-frontend/src/utils/unleash.ts
+++ din-uforetrygd-frontend/src/utils/unleash.ts
@@ -3,19 +3,21 @@ import { evaluateFlags, flagsClient, getDefinitions } from '@unleash/nextjs'
 import { after } from 'next/server'
 
 export const isEnabled = async (toggle: string): Promise<boolean> => {
-  const isDev = process.env.NODE_ENV !== 'production'
-  if (isDev) {
-    return localToggles[toggle] ?? false
-  }
-
   const cookieStore = await cookies()
   const sessionId = cookieStore.get(unleashSessionIdKey)?.value
 
-  const definitions = await getDefinitions({
-    fetchOptions: {
-      next: { revalidate: 15 }, // cache i 15 sek
-    },
-  })
+  let definitions
+  try {
+    definitions = await getDefinitions({
+      fetchOptions: {
+        next: { revalidate: 15 }, // cache i 15 sek
+        signal: AbortSignal.timeout(3000),
+      },
+    })
+  } catch (e) {
+    console.error('Henting av feature toggles fra Unleash feilet', e)
+    return false
+  }
 
   const { toggles } = evaluateFlags(definitions, {
     sessionId,
