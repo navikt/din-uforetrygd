5a6183b29737924d37109e67260995e381b7c2f1
André Mossige <andre.mossige@nav.no>
2026-01-16 09:41:52 +0100

    Ikke vis noe data vis toggle er slått av

--- din-uforetrygd-frontend/src/app/page.tsx
+++ din-uforetrygd-frontend/src/app/page.tsx
@@ -36,7 +36,7 @@ const Home: React.FC<IHomeProps> = async ({ searchParams }) => {
 
     return (
       <>
-        {toggleErSlåttPå ? <div>Toggle er slått på!</div> : <div>Toggle slått av</div>}
+        {toggleErSlåttPå && <div>Toggle er slått på!</div>}
         <TaskAnalytics id="03419" shouldRun={mode === 'borger'} />
         <VeilederBorgerinformasjon pid={uforetrygdResponse.pid} />
         <EventProvider>
