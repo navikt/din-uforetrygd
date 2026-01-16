b7a7444dec66f82002ae154c4c3ceee4b60cfe90
André Mossige <andre.mossige@nav.no>
2026-01-16 11:34:59 +0100

    Reintroduser kode jeg fjerna uten vilje

--- din-uforetrygd-frontend/src/utils/unleash.ts
+++ din-uforetrygd-frontend/src/utils/unleash.ts
@@ -3,6 +3,11 @@ import { evaluateFlags, flagsClient, getDefinitions } from '@unleash/nextjs'
 import { after } from 'next/server'
 
 export const isEnabled = async (toggle: string): Promise<boolean> => {
+  const isDev = process.env.NODE_ENV !== 'production'
+  if (isDev) {
+    return localToggles[toggle] ?? false
+  }
+
   const cookieStore = await cookies()
   const sessionId = cookieStore.get(unleashSessionIdKey)?.value
 
