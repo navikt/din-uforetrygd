4774542ef839c00f483fcac6d864cf4537b95c4f
eirikstadheim <eirik.stadheim@nav.no>
2026-01-21 18:29:35 +0100

    UFO-194: oppdaterer mockdata ift nye endepunkter

--- din-uforetrygd-frontend/mock/mockSaksoversiktData.ts
+++ din-uforetrygd-frontend/mock/mockSaksoversiktData.ts
@@ -3,23 +3,68 @@ import { components } from '../src/api/api'
 export const mockSaksoversiktData: Record<string, components['schemas']['SaksoversiktResponse']> = {
   default: {
     aktivBehandling: {
-      visningstittel: 'Søknad om uføretrygd',
-      status: 'TIL_BEHANDLING',
+      tittel: 'Søknad om uføretrygd',
       mottattDato: '2024-10-01',
       ferdigstiltDato: undefined,
+      avslag: false,
+      etteroppgjor: undefined,
+      steg: [
+        {
+          aktiv: true,
+          utfort: false,
+          tittel: "Søknad mottatt"
+        },
+        {
+          aktiv: false,
+          utfort: false,
+          tittel: "Søknad innvilget",
+          undertekst: "Det er bra"
+        }
+      ],
+      vedtakId: 123
     },
     avsluttedeBehandlinger: [
       {
-        visningstittel: 'Revurdering av uføretrygd',
-        status: 'TIL_BEHANDLING',
+        tittel: 'Revurdering av uføretrygd',
         mottattDato: '2023-05-15',
         ferdigstiltDato: '2023-06-20',
+        avslag: false,
+        etteroppgjor: undefined,
+        steg: [
+          {
+            aktiv: false,
+            utfort: true,
+            tittel: "Søknad mottatt"
+          },
+          {
+            aktiv: true,
+            utfort: false,
+            tittel: "Søknad innvilget",
+            undertekst: "Det er bra"
+          }
+        ],
+        vedtakId: 123
       },
       {
-        visningstittel: 'Etteroppgjør',
-        status: 'TIL_BEHANDLING',
+        tittel: 'Etteroppgjør',
         mottattDato: '2024-05-15',
         ferdigstiltDato: '2024-06-20',
+        avslag: false,
+        etteroppgjor: undefined,
+        steg: [
+          {
+            aktiv: false,
+            utfort: true,
+            tittel: "Søknad mottatt"
+          },
+          {
+            aktiv: false,
+            utfort: true,
+            tittel: "Søknad innvilget",
+            undertekst: "Det er bra"
+          }
+        ],
+        vedtakId: 1234
       },
     ],
   },
@@ -27,10 +72,25 @@ export const mockSaksoversiktData: Record<string, components['schemas']['Saksove
     aktivBehandling: undefined,
     avsluttedeBehandlinger: [
       {
-        visningstittel: 'Søknad om uføretrygd',
-        status: 'TIL_BEHANDLING',
+        tittel: 'Søknad om uføretrygd',
         mottattDato: '2023-01-01',
         ferdigstiltDato: '2023-02-15',
+        avslag: false,
+        etteroppgjor: undefined,
+        steg: [
+          {
+            aktiv: false,
+            utfort: true,
+            tittel: "Søknad mottatt"
+          },
+          {
+            aktiv: false,
+            utfort: true,
+            tittel: "Søknad innvilget",
+            undertekst: "Det er bra"
+          }
+        ],
+        vedtakId: 1234
       },
     ],
   },
