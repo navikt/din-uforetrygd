9cf5b1efde104a6a195a29adf6357e961470d710
eirikstadheim <eirik.stadheim@nav.no>
2026-02-03 09:51:39 +0100

    UFO-196: fjerner ingen sak-visning når ny funksjonalitet er slått på Co-authored-by: Hanne Æsøy Nes <hanne.esoy.nes@nav.no> Co-authored-by: André Mossige <andre.mossige@nav.no>

--- din-uforetrygd-frontend/src/sections/UforeStatusGuidePanel/index.tsx
+++ din-uforetrygd-frontend/src/sections/UforeStatusGuidePanel/index.tsx
@@ -1,6 +1,7 @@
 import { Visningskriterier } from '@/const'
 import { UforestatusGuidePanelView } from './UforestatusGuidePanelView'
 import { getUrl } from '@/utils/getUrl'
+import { isEnabled } from '@/utils/unleash'
 
 interface IUforestatusGuideProps {
   visningskriterier: Visningskriterier[]
@@ -9,13 +10,16 @@ interface IUforestatusGuideProps {
 const UforestatusGuidePanel: React.FC<IUforestatusGuideProps> = async (props) => {
   const dittLokaleNavKontorLenke = await getUrl({ urlFromEnv: 'LINK_DITT_LOKALE_NAV_KONTOR' })
   const saksbehandlingstiderLenke = await getUrl({ urlFromEnv: 'LINK_SAKSBEHANDLINGSTIDER_UFORETRYGD' })
+  const visBehandling = await isEnabled('din.uforetrygd.forside.behandling')
 
   return (
-    <UforestatusGuidePanelView
-      visningskriterier={props.visningskriterier}
-      dittLokaleNavKontorLenke={dittLokaleNavKontorLenke}
-      saksbehandlingstiderLenke={saksbehandlingstiderLenke}
-    />
+    !visBehandling && (
+      <UforestatusGuidePanelView
+        visningskriterier={props.visningskriterier}
+        dittLokaleNavKontorLenke={dittLokaleNavKontorLenke}
+        saksbehandlingstiderLenke={saksbehandlingstiderLenke}
+      />
+    )
   )
 }
 
