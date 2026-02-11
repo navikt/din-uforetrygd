03a927651b944856622039641a9fc3444fc9b11d
André Mossige <andremossige@fastmail.com>
2026-02-11 08:50:40 +0100

    Sletter ubrukt Prometheus-metrikk (#223)
    * Sletter ubrukt Prometheus-metrikk
    
    * Fjern ubrukt metode som kun ble brukt i test

--- din-uforetrygd-backend/src/main/kotlin/no/nav/dinuforetrygd/vergemaal/VergemaalMetricsCounter.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.dinuforetrygd.vergemaal
-
-import io.micrometer.core.instrument.Metrics
-
-const val EVENT_NAME = "loginlevel_vergemaal_and_country_counter"
-
-fun countEvent(result: String) = Metrics.counter(EVENT_NAME, "result", result).increment()
