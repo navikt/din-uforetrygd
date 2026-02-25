46c873657fec9066cc8aa3f18490f70a909b05f6
Hanne Æsøy Nes <hanne.esoy.nes@nav.no>
2026-02-25 15:19:33 +0100

    Fikser feil manuelt

--- din-uforetrygd-frontend/src/utils/unleash.ts
+++ din-uforetrygd-frontend/src/utils/unleash.ts
@@ -1,44 +1,40 @@
-import { cookies } from 'next/headers'
 import { evaluateFlags, flagsClient, getDefinitions } from '@unleash/nextjs'
+import { cookies } from 'next/headers'
 import { after } from 'next/server'
 
 export const isEnabled = async (toggle: string): Promise<boolean> => {
-  /* const isDev = process.env.NODE_ENV !== 'production'
+	/* const isDev = process.env.NODE_ENV !== 'production'
    if (isDev) {
      return localToggles[toggle] ?? false
    }*/
 
-  const cookieStore = await cookies()
-  const sessionId = cookieStore.get(unleashSessionIdKey)?.value
+	const cookieStore = await cookies()
+	const sessionId = cookieStore.get(unleashSessionIdKey)?.value
 
-  let definitions
-  try {
-    definitions = await getDefinitions({
-      fetchOptions: {
-        next: { revalidate: 15 }, // cache i 15 sek
-        signal: AbortSignal.timeout(3000),
-      },
-    })
-  } catch (e) {
-    console.error('Henting av feature toggles fra Unleash feilet', e)
-    return false
-  }
+	const definitions = await getDefinitions({
+		fetchOptions: {
+			next: { revalidate: 15 }, // cache i 15 sek
+			signal: AbortSignal.timeout(3000),
+		},
+	}).catch((e) => {
+		console.error('Henting av feature toggles fra Unleash feilet', e)
+		return null
+	})
 
-  const { toggles } = evaluateFlags(definitions, {
-    sessionId,
-  })
-  const flags = flagsClient(toggles)
-  const isEnabled = flags.isEnabled(toggle)
+	if (!definitions) return false
 
-  // Ikke blokkerende rapportering tilbake til Unleash
-  after(async () => flags.sendMetrics())
+	const { toggles } = evaluateFlags(definitions, { sessionId })
+	const flags = flagsClient(toggles)
 
-  return isEnabled
+	// Ikke blokkerende rapportering tilbake til Unleash
+	after(async () => flags.sendMetrics())
+
+	return flags.isEnabled(toggle)
 }
 
 export const unleashSessionIdKey = 'unleash-session-id'
 
-type LocalToggles = Record<string, boolean>
-const localToggles: LocalToggles = {
-  'test-toggle': true,
-}
+// type LocalToggles = Record<string, boolean>
+// const localToggles: LocalToggles = {
+// 	'test-toggle': true,
+// }
