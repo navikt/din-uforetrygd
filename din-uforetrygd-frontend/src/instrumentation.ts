0a4881fce04136069e18c84508867b0f23e7ca0c
Hanne Æsøy Nes <hanne.esoy.nes@nav.no>
2026-02-25 09:46:29 +0100

    Logger next-error bedre Co-authored-by: André Mossige <andre.mossige@nav.no>

---
+++ din-uforetrygd-frontend/src/instrumentation.ts
@@ -0,0 +1,17 @@
+import logger from './utils/logger'
+
+export function onRequestError(
+  err: { digest?: string } & Error,
+  request: { path: string; method: string; headers: Record<string, string> },
+) {
+  logger.error({
+    message: err.message,
+    http: {
+      request: {
+        method: request.method,
+        path: request.path,
+        host: request.headers['host'] ?? '',
+      },
+    },
+  })
+}
