c65c70d8868a1df4be4e4d93d279dffb3210724e
André Mossige <andre.mossige@nav.no>
2026-02-27 10:48:16 +0100

    Logger om faro settes opp riktig
    Co-authored-by: Hanne Æsøy Nes <hanne.esoy.nes@nav.no>

--- din-uforetrygd-frontend/src/utils/faro.ts
+++ din-uforetrygd-frontend/src/utils/faro.ts
@@ -1,11 +1,16 @@
 import { type Faro, getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk'
 import getEnv from '@/utils/env'
+import logger from '@/utils/logger'
 
 let faro: Faro | null = null
 
 export function initFaro(): void {
-  if (typeof window === 'undefined' || faro !== null) return
+  if (typeof window === 'undefined' || faro !== null) {
+    logger.info({ message: `Setter ikke opp faro siden window er null eller faro er ulik null` })
+    return
+  }
   const faroUrl = getEnv('FARO_URL')
+  logger.info({ message: `Setter opp Faro med URL ${faroUrl}` })
   faro = initializeFaro({
     url: faroUrl,
     app: {
@@ -13,4 +18,5 @@ export function initFaro(): void {
     },
     instrumentations: [...getWebInstrumentations()],
   })
+  logger.info({ message: 'Faro initialized success' })
 }
