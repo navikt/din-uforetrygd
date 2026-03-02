a05c2e1731a2bcc530169dd6387493d4162fcbb2
André Mossige <andre.mossige@nav.no>
2026-03-02 15:32:51 +0100

    Fiks promise inni promise anti pattern

--- din-uforetrygd-frontend/src/api/getOboToken.ts
+++ din-uforetrygd-frontend/src/api/getOboToken.ts
@@ -2,29 +2,26 @@ import { getToken, requestOboToken, validateToken } from '@navikt/oasis'
 import { headers } from 'next/headers'
 import getEnv from '@/utils/env'
 
-const getOboToken = async () => {
-  // biome-ignore lint/suspicious/noAsyncPromiseExecutor: TODO bør refaktoreres
-  return new Promise(async (resolve, reject) => {
-    if (process.env.NODE_ENV !== 'production') {
-      resolve(process.env.ACCESS_TOKEN)
-    }
-    const clientHeaders = await headers()
-    const token = getToken(clientHeaders)
-    if (!token) {
-      return reject('Missing wonderwall cookie')
-    }
-    const validation = await validateToken(token)
-    if (!validation.ok) {
-      return reject(`Validation failed: ${validation.error}`)
-    }
-    const obo = await requestOboToken(token, getEnv('DIN_UFORETRYGD_BACKEND_SCOPE')!)
+const getOboToken = async (): Promise<string> => {
+  if (process.env.NODE_ENV !== 'production') {
+    return process.env.ACCESS_TOKEN!
+  }
+  const clientHeaders = await headers()
+  const token = getToken(clientHeaders)
+  if (!token) {
+    throw new Error('Missing wonderwall cookie')
+  }
+  const validation = await validateToken(token)
+  if (!validation.ok) {
+    throw validation.error
+  }
+  const obo = await requestOboToken(token, getEnv('DIN_UFORETRYGD_BACKEND_SCOPE')!)
 
-    if (!obo.ok) {
-      return reject(`OBO Exchange failed: ${obo.error}`)
-    }
+  if (!obo.ok) {
+    throw obo.error
+  }
 
-    resolve(obo.token)
-  })
+  return obo.token
 }
 
 export default getOboToken
