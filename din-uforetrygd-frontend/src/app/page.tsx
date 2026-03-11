70c3cb0769ed93a799902f121ad359b041354b1b
tordbjorn.wang.eriksen <tordbjorn.wang.eriksen@nav.no>
2026-03-11 15:09:22 +0100

    UFO-276 Ny snarvei i snarveier for Regelverksendringer 2026, med unleash-toggle som ikke viser snarvei etter 01.01.2027.

--- din-uforetrygd-frontend/src/app/page.tsx
+++ din-uforetrygd-frontend/src/app/page.tsx
@@ -1,55 +1,58 @@
-import { Alert, BodyLong, Heading, VStack } from '@navikt/ds-react'
-import { initate } from '@/api/endpoints'
-import { TaskAnalytics } from '@/components/TaskAnalytics/TaskAnalytics'
-import { VeilederBorgerinformasjon } from '@/components/VeilederBorgerinformasjon/VeilederBorgerinformasjon'
-import type { Innloggingstype, Visningskriterier } from '@/const'
-import { KanVaereAktueltForDeg } from '@/sections/KanVaereAktueltForDeg/KanVaereAktueltForDeg'
-import { MeldeFra } from '@/sections/MeldeFra/MeldeFra'
-import { RelevanteSoknader } from '@/sections/RelevanteSoknader/RelevanteSoknader'
-import getEnv from '@/utils/env'
-import { getVisningskriterier } from '@/utils/getVisningskriterier/getVisningskriterier'
-import { resolveErrorText } from '@/utils/resolveErrorText/resolveErrorText'
-import './layout.css'
-import type React from 'react'
-import { LukkbarAlert } from '@/components/Alert/LukkbarAlert'
-import Brødsmulesti from '@/components/Brødsmulesti/Brødsmulesti'
-import { DittVedtak } from '@/sections/DittVedtak/DittVedtak'
-import { ForsideBehandlingKort } from '@/sections/ForsideBehandling/ForsideBehandlingKort'
-import { toForsideBehandling } from '@/sections/ForsideBehandling/forsideBehandlingUtil'
-import { InntektSnarveier } from '@/sections/InntektSnarveier/InntektSnarveier'
-import { InterneLenker } from '@/sections/InterneLenker/InterneLenker'
-import { Snarveier } from '@/sections/Snarveier/Snarveier'
-import EventProvider from '@/utils/dataContextProvider/EventContextProvider'
+import { Alert, Heading, VStack } from "@navikt/ds-react";
+import { initate } from "@/api/endpoints";
+import { TaskAnalytics } from "@/components/TaskAnalytics/TaskAnalytics";
+import { VeilederBorgerinformasjon } from "@/components/VeilederBorgerinformasjon/VeilederBorgerinformasjon";
+import type { Innloggingstype, Visningskriterier } from "@/const";
+import { KanVaereAktueltForDeg } from "@/sections/KanVaereAktueltForDeg/KanVaereAktueltForDeg";
+import { MeldeFra } from "@/sections/MeldeFra/MeldeFra";
+import { RelevanteSoknader } from "@/sections/RelevanteSoknader/RelevanteSoknader";
+import getEnv from "@/utils/env";
+import { getVisningskriterier } from "@/utils/getVisningskriterier/getVisningskriterier";
+import { resolveErrorText } from "@/utils/resolveErrorText/resolveErrorText";
+import "./layout.css";
+import type React from "react";
+import Brødsmulesti from "@/components/Brødsmulesti/Brødsmulesti";
+import { DittVedtak } from "@/sections/DittVedtak/DittVedtak";
+import { ForsideBehandlingKort } from "@/sections/ForsideBehandling/ForsideBehandlingKort";
+import { toForsideBehandling } from "@/sections/ForsideBehandling/forsideBehandlingUtil";
+import { InntektSnarveier } from "@/sections/InntektSnarveier/InntektSnarveier";
+import { InterneLenker } from "@/sections/InterneLenker/InterneLenker";
+import { Snarveier } from "@/sections/Snarveier/Snarveier";
+import EventProvider from "@/utils/dataContextProvider/EventContextProvider";
 
 interface IHomeProps {
-  searchParams: Promise<{ pid?: string }>
+  searchParams: Promise<{ pid?: string }>;
 }
 
 const Home: React.FC<IHomeProps> = async ({ searchParams }) => {
-  const params = await searchParams
-  const initResponse = await initate(params.pid)
-  const uforetrygdResponse = initResponse.uforetrygdResponse
+  const params = await searchParams;
+  const initResponse = await initate(params.pid);
+  const uforetrygdResponse = initResponse.uforetrygdResponse;
 
   if (uforetrygdResponse) {
-    const visningskriterier: Visningskriterier[] = getVisningskriterier(uforetrygdResponse)
-    const mode = getEnv('MODE')
-    const uforesak = uforetrygdResponse.sak
+    const visningskriterier: Visningskriterier[] =
+      getVisningskriterier(uforetrygdResponse);
+    const mode = getEnv("MODE");
+    const uforesak = uforetrygdResponse.sak;
 
     return (
       <>
-        <TaskAnalytics id="03419" shouldRun={mode === 'borger'} />
+        <TaskAnalytics id="03419" shouldRun={mode === "borger"} />
         <VeilederBorgerinformasjon pid={uforetrygdResponse.pid} />
         <EventProvider>
-          <VStack gap="space-12" className={'tittel-wrapper'}>
+          <VStack gap="space-12" className={"tittel-wrapper"}>
             <Brødsmulesti mode={mode} />
             <Heading size="xlarge" level="1">
               Din uføretrygd
             </Heading>
           </VStack>
+
           <ForsideBehandlingKort
             behandling={
               initResponse.uforetrygdResponse.behandling
-                ? toForsideBehandling(initResponse.uforetrygdResponse.behandling)
+                ? toForsideBehandling(
+                    initResponse.uforetrygdResponse.behandling,
+                  )
                 : null
             }
             visningskriterier={visningskriterier}
@@ -57,7 +60,9 @@ const Home: React.FC<IHomeProps> = async ({ searchParams }) => {
           <InntektSnarveier
             visningskriterier={visningskriterier}
             pid={params.pid}
-            innloggingstype={uforetrygdResponse.innloggingstype as Innloggingstype}
+            innloggingstype={
+              uforetrygdResponse.innloggingstype as Innloggingstype
+            }
           ></InntektSnarveier>
           <DittVedtak
             pid={params.pid}
@@ -71,19 +76,26 @@ const Home: React.FC<IHomeProps> = async ({ searchParams }) => {
             pid={params.pid}
             journalposter={uforetrygdResponse.journalposter}
           ></InterneLenker>
-          <Snarveier visningskriterier={visningskriterier} pid={params.pid} uforetrygdResponse={uforetrygdResponse} />
+          <Snarveier
+            visningskriterier={visningskriterier}
+            pid={params.pid}
+            uforetrygdResponse={uforetrygdResponse}
+          />
           <MeldeFra visningskriterier={visningskriterier} />
           <RelevanteSoknader
             visningskriterier={visningskriterier}
             innloggingstype={uforetrygdResponse.innloggingstype}
           />
           <KanVaereAktueltForDeg visningskriterier={visningskriterier} />
-          <div className={'ux-signals-container'}>
-            <div data-uxsignals-embed={'panel-u5y48zl9t7'} className={'ux-signals'}></div>
+          <div className={"ux-signals-container"}>
+            <div
+              data-uxsignals-embed={"panel-u5y48zl9t7"}
+              className={"ux-signals"}
+            ></div>
           </div>
         </EventProvider>
       </>
-    )
+    );
   } else {
     return (
       <section className="main-content">
@@ -91,8 +103,8 @@ const Home: React.FC<IHomeProps> = async ({ searchParams }) => {
           {resolveErrorText(initResponse.backendError?.message)}
         </Alert>
       </section>
-    )
+    );
   }
-}
+};
 
-export default Home
+export default Home;
