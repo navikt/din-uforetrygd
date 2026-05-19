d0e768379dcb70e918a9093ffe93a4197d82480e
Hanne Æsøy Nes <hanne.esoy.nes@nav.no>
2026-05-19 13:37:09 +0200

    Legger til unleash i serveren for steng for g-regulering

--- din-uforetrygd-frontend/server/server.ts
+++ din-uforetrygd-frontend/server/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import next from 'next'
+import { initialize } from 'unleash-client'
 import { stengForReguleringMiddleware } from '@navikt/steng-for-regulering/express'
 
 const isDev = process.env.NODE_ENV !== 'production'
@@ -8,10 +9,32 @@ const app = next({ dev: isDev })
 const handle = app.getRequestHandler()
 const server = express()
 
+const unleashUrl = process.env.UNLEASH_SERVER_API_URL
+const unleashToken = process.env.UNLEASH_SERVER_API_TOKEN
+const unleashEnv = process.env.UNLEASH_SERVER_API_ENV
+
+const unleash = initialize({
+  disableAutoStart: !(unleashToken && unleashUrl),
+  url: `${unleashUrl}/api`,
+  appName: 'din-uforetrygd-frontend',
+  environment: unleashEnv,
+  customHeaders: {
+    Authorization: unleashToken ?? '',
+  },
+})
+
+unleash.on('synchronized', () => {
+  console.log('Unleash synchronized')
+})
+
+unleash.on('error', (err) => {
+  console.error('Unleash error:', err)
+})
+
 app
   .prepare()
   .then(() => {
-    server.use(stengForReguleringMiddleware({ env: isDev ? 'dev' : 'prod' }))
+    server.use(stengForReguleringMiddleware({ env: isDev ? 'dev' : 'prod', unleashClient: unleash }))
     server.get(/(.*)/, (req, res) => {
       return handle(req, res)
     })
