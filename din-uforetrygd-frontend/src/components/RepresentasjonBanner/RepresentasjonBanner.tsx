bb1f685d599c2e63bac757e14183a0ebe792894c
Sindre Rasmussen <sindre.rasmussen@nav.no>
2026-06-11 10:59:10 +0200

    PEB-1603: Added breadcrumbs in representasjon-banner.

--- din-uforetrygd-frontend/src/components/RepresentasjonBanner/RepresentasjonBanner.tsx
+++ din-uforetrygd-frontend/src/components/RepresentasjonBanner/RepresentasjonBanner.tsx
@@ -9,6 +9,10 @@ export const RepresentasjonBanner = () => {
         className={styles.representasjonBanner}
         representasjonstyper="UFORETRYGD_LES,UFORETRYGD_SKRIV,VERGE_UFORETRYGD_LES,VERGE_UFORETRYGD_SKRIV"
         redirectTo={`${window.location.origin}/uforetrygd/selvbetjening`}
+        breadcrumbs={JSON.stringify([
+            { url: "https://www.nav.no/minside", title: "Min side" },
+            { url: "https://www.nav.no/uforetrygd/selvbetjening", title: "Din uføretrygd" },
+        ])}
       ></representasjon-banner>
     </div>
   )
