ad63de0b8c922a995f65dca3fd55f2a35847663c
Hanne Æsøy Nes <48721817+hanneanes@users.noreply.github.com>
2026-06-22 15:08:19 +0200

    UFO-530: ExpansionCard egen komponent sånn at også brukes i FinnUtMer (#495)

---
+++ din-uforetrygd-frontend/src/components/ExpansionCardMedIkon/ExpansionCardMedIkon.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { HStack } from '@navikt/ds-react'
+import { ExpansionCard } from '@navikt/ds-react/ExpansionCard'
+import type { ReactNode } from 'react'
+
+interface Props {
+  ikon: ReactNode
+  tittel: string
+  undertittel?: string
+  children: ReactNode
+}
+export default function ExpansionCardMedIkon({ ikon, tittel, undertittel = '', children }: Props) {
+  return (
+    <ExpansionCard aria-label={tittel} size="small">
+      <ExpansionCard.Header>
+        <HStack wrap={false} gap="space-16" align="center">
+          <div>{ikon}</div>
+          <div>
+            <ExpansionCard.Title as="h4" size="small">
+              {tittel}
+            </ExpansionCard.Title>
+            {undertittel && <ExpansionCard.Description>{undertittel}</ExpansionCard.Description>}
+          </div>
+        </HStack>
+      </ExpansionCard.Header>
+      <ExpansionCard.Content>{children}</ExpansionCard.Content>
+    </ExpansionCard>
+  )
+}
