e46b8193aba20d85e5adbfe0b36d8856cf7235f4
Hanne Æsøy Nes <48721817+hanneanes@users.noreply.github.com>
2026-06-22 15:52:40 +0200

    UFO-603: Dine muligheter desktop-versjon (#497)
    * UFO-603: Dine muligheter desktop-versjon
    
    * UFO-603: Finn ut mer desktop-versjon

--- din-uforetrygd-frontend/src/sections/DineMuligheter/DineMuligheter.tsx
+++ din-uforetrygd-frontend/src/sections/DineMuligheter/DineMuligheter.tsx
@@ -7,7 +7,7 @@ import {
   PersonGroupFillIcon,
   PiggybankFillIcon,
 } from '@navikt/aksel-icons'
-import { BodyLong, BodyShort, Button, Heading, HStack, Link, List, VStack } from '@navikt/ds-react'
+import { BodyLong, BodyShort, Button, Heading, HGrid, HStack, Link, List, VStack } from '@navikt/ds-react'
 import Brødsmulesti from '@/components/Brødsmulesti/Brødsmulesti'
 import ExpansionCardMedIkon from '@/components/ExpansionCardMedIkon/ExpansionCardMedIkon'
 import getEnv from '@/utils/env'
@@ -17,23 +17,23 @@ const DineMuligheter = () => {
   const mode = getEnv('MODE')
 
   return (
-    <>
-      <section>
-        <VStack gap="space-24">
-          <VStack gap="space-12">
-            <Brødsmulesti mode={mode} />
-            <HStack gap={'space-12'} align="center">
-              <HandShakeHeartFillIcon color={'#7342B6'} fontSize={'3rem'} />
-              <Heading size="large" level="2">
-                Dine muligheter
-              </Heading>
-            </HStack>
-            <BodyShort>
-              I tillegg til uføretrygden kan du jobbe, studere eller gjøre andre aktiviteter hvis du har mulighet. Du
-              beholder retten til uføretrygd.
-            </BodyShort>
-          </VStack>
-          <VStack gap={'space-12'}>
+    <VStack className={styles.dineMuligheterWrapper} gap="space-48">
+      <VStack gap="space-12" maxWidth={'678px'}>
+        <Brødsmulesti mode={mode} />
+        <HStack gap={'space-12'} align="center">
+          <HandShakeHeartFillIcon color={'#7342B6'} fontSize={'3rem'} />
+          <Heading size="large" level="2">
+            Dine muligheter
+          </Heading>
+        </HStack>
+        <BodyShort>
+          I tillegg til uføretrygden kan du jobbe, studere eller gjøre andre aktiviteter hvis du har mulighet. Du
+          beholder retten til uføretrygd.
+        </BodyShort>
+      </VStack>
+      <div>
+        <div className={styles.fargeContainer + ' ' + styles.graContainer}>
+          <HGrid gap="space-24" columns={{ md: 2 }} className={styles.fargeContainerContent}>
             <ExpansionCardMedIkon
               tittel="Vi kan hjelpe deg"
               ikon={<PersonGroupFillIcon color={'#7342B6'} fontSize={'3rem'} />}
@@ -85,9 +85,7 @@ const DineMuligheter = () => {
             </ExpansionCardMedIkon>
             <ExpansionCardMedIkon
               ikon={<EarthFillIcon color={'#7342B6'} fontSize={'3rem'} />}
-              tittel="
-                  Du kan ordne ting selv
-            "
+              tittel="Du kan ordne ting selv"
             >
               <VStack gap="space-12">
                 <BodyShort>
@@ -158,41 +156,50 @@ const DineMuligheter = () => {
                 </List>
               </VStack>
             </ExpansionCardMedIkon>
+            <Button variant={'primary'} as="a" href="dine-muligheter/finn-ut-mer">
+              Snakk med oss
+            </Button>
+          </HGrid>
+        </div>
+        <div className={styles.fargeContainer + ' ' + styles.rosaContainer}>
+          <VStack gap="space-12" className={styles.fargeContainerContent}>
+            <Heading size="large" level="3">
+              Sara (24) fikk hjelp til å finne jobb
+            </Heading>
+            <BodyLong size="medium">
+              Historie fra virkeligheten: slik fikk uføre Sara hjelp til å finne deltidsjobb
+            </BodyLong>
+            {/*<video controls>*/}
+            {/*  <source src="/videos/sara-jobb.mp4" type="video/mp4" />*/}
+            {/*</video>*/}
           </VStack>
-          <Button variant={'primary'} as="a" href="dine-muligheter/finn-ut-mer">
-            Snakk med oss
-          </Button>
-        </VStack>
-      </section>
-      <section className={styles.rosaContainer}>
-        <VStack gap="space-12" className={styles.rosaContainerContent}>
-          <Heading size="large" level="3">
-            Sara (24) fikk hjelp til å finne jobb
-          </Heading>
-          <BodyLong size="medium">
-            Historie fra virkeligheten: slik fikk uføre Sara hjelp til å finne deltidsjobb
-          </BodyLong>
-          {/*<video controls>*/}
-          {/*  <source src="/videos/sara-jobb.mp4" type="video/mp4" />*/}
-          {/*</video>*/}
-        </VStack>
-      </section>
-      <section>
-        <Heading size="large" level="3">
-          Velg ulik inntekt for å se hvordan det kan påvirke uføretrygden til Sara
-        </Heading>
-      </section>
-      <section className={styles.rosaContainer} style={{ paddingBottom: '48px' }}>
-        <VStack gap="space-16" className={styles.rosaContainerContent}>
-          <Heading size="large" level="3">
-            Vi kan hjelpe deg med å utforske mulighetene dine.
-          </Heading>
-          <Button variant="secondary" as="a" href="/finn-ut-mer">
-            Snakk med oss
-          </Button>
-        </VStack>
-      </section>
-    </>
+        </div>
+      </div>
+      <Heading size="large" level="3">
+        Velg ulik inntekt for å se hvordan det kan påvirke uføretrygden til Sara
+      </Heading>
+      <div className={styles.fargeContainer + ' ' + styles.rosaContainer}>
+        <HGrid
+          gap={{ xs: 'space-24', md: 'space-48', lg: 'space-64' }}
+          className={styles.fargeContainerContent}
+          columns={{ md: 2 }}
+        >
+          <VStack gap="space-12">
+            <Heading size="large" level="3">
+              Vi kan hjelpe deg med å utforske mulighetene dine.
+            </Heading>
+            <BodyShort>
+              Ta kontakt med en veileder, det er uforpliktende og du beholder retten til uføretrygd.{' '}
+            </BodyShort>
+          </VStack>
+          <VStack gap="space-12" justify="center">
+            <Button as="a" href="dine-muligheter/finn-ut-mer">
+              Snakk med oss
+            </Button>
+          </VStack>
+        </HGrid>
+      </div>
+    </VStack>
   )
 }
 
