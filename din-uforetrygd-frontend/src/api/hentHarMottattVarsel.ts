7b346ecfd31f794605ada48631cfffc528dbafb2
Hanne Æsøy Nes <48721817+hanneanes@users.noreply.github.com>
2026-08-14 09:56:50 +0200

    UFO-605: Endringer etter endringer i ufore-varsler (#572)
    * UFO-605: Endringer etter endringer i ufore-varsler
    
    Co-authored-by: André Mossige <andre.mossige@nav.no>
    
    * Legger på cookie
    
    * Legger til content type for veileder
    
    ---------
    
    Co-authored-by: André Mossige <andre.mossige@nav.no>

--- din-uforetrygd-frontend/src/api/hentHarMottattVarsel.ts
+++ din-uforetrygd-frontend/src/api/hentHarMottattVarsel.ts
@@ -1,18 +1,36 @@
+import { headers } from 'next/headers'
+import { getFullmaktCookie } from '@/api/getFullmaktCookie'
 import { getUforeVarslerOboToken } from '@/api/getOboToken'
+import getEnv from '@/utils/env'
 
 export const hentHarMottattVarsel = async (): Promise<boolean> => {
+  const mode = getEnv('MODE') as 'borger' | 'veileder'
+
   const oboToken = await getUforeVarslerOboToken().catch((error) => {
     console.error('Error: ', error)
     return
   })
 
-  const headers: Record<string, string> = {
+  const nextHeaders = await headers()
+  const body = mode === 'veileder' ? { fnr: nextHeaders.get('x-kryptert-pid') } : undefined
+
+  const fullmaktCookie = await getFullmaktCookie()
+
+  const headere: Record<string, string> = {
     Authorization: `Bearer ${oboToken}`,
+    Cookie: fullmaktCookie as string,
+    'Content-Type': 'application/json',
   }
 
   const url = process.env.NODE_ENV !== 'development' ? process.env.UFORE_VARSLER : 'http://localhost:8080'
 
-  const response = await fetch(`${url}/api/varsler/status`, { headers, cache: 'no-store' })
+  const response = await fetch(`${url}/api/varsler/status`, {
+    method: 'POST',
+    headers: headere,
+    cache: 'no-store',
+    body: body && JSON.stringify(body),
+  })
+
   if (!response.ok) return false
 
   const json = await response.json()
