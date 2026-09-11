c370be46432b7f6659f9d7b2202b0c9400c4063d
ludviklund <lulu@computas.com>
2026-09-11 14:34:39 +0200

    UFO-748: lagt til @nais/apm og fjernet gammel Fargo (som nå er inkludert)

--- din-uforetrygd-frontend/src/app/layout.tsx
+++ din-uforetrygd-frontend/src/app/layout.tsx
@@ -1,17 +1,20 @@
-import { Theme } from '@navikt/ds-react'
-import { fetchDecoratorReact } from '@navikt/nav-dekoratoren-moduler/ssr'
-import Script from 'next/script'
-import '../global.css'
-import './layout.css'
-import '@navikt/ds-tokens/dist/tokens.css'
-import '@navikt/ds-css'
-import { connection } from 'next/server'
 import Brødsmulesti from '@/components/Brødsmulesti/Brødsmulesti'
 import { FullmaktModal } from '@/components/FullmaktModal/FullmaktModal'
 import RepresentasjonBanner from '@/components/RepresentasjonBanner'
 import { VeilederBanner } from '@/components/VeilederBanner/VeilederBanner'
 import { env } from '@/env'
-import InitializeFaro from '@/utils/faro/faro'
+import { NaisMetaTags } from '@nais/apm/react'
+import '@navikt/ds-css'
+import { Theme } from '@navikt/ds-react'
+import '@navikt/ds-tokens/dist/tokens.css'
+import { fetchDecoratorReact } from '@navikt/nav-dekoratoren-moduler/ssr'
+import Script from 'next/script'
+import { connection } from 'next/server'
+import { Suspense } from 'react'
+import '../global.css'
+import { ApmErrorBoundary } from './ApmErrorBoundary'
+import { ApmRouteTracker } from './ApmRouteTracker'
+import './layout.css'
 
 const RootLayout = async ({ children }: Readonly<{ children: React.ReactNode }>) => {
   // Miljøvariabler for borger og veileder i injectes runtime til samme bundle.
@@ -34,15 +37,20 @@ const RootLayout = async ({ children }: Readonly<{ children: React.ReactNode }>)
       <html lang="no">
         <head>
           <title>Din uføretrygd - Veileder</title>
+          <NaisMetaTags />
         </head>
         <body>
           <Theme>
-            <VeilederBanner />
-            <main className="main-content" id="maincontent" tabIndex={-1}>
-              <Brødsmulesti mode="veileder" />
-              {children}
-            </main>
-            <InitializeFaro url={env('FARO_URL')} appName={env('NAIS_APP_NAME')} />
+            <ApmErrorBoundary fallback={<p>Noe gikk galt.</p>}>
+              <VeilederBanner />
+              <main className="main-content" id="maincontent" tabIndex={-1}>
+                <Brødsmulesti mode="veileder" />
+                {children}
+                <Suspense fallback={null}>
+                  <ApmRouteTracker />
+                </Suspense>
+              </main>
+            </ApmErrorBoundary>
           </Theme>
         </body>
       </html>
@@ -54,24 +62,29 @@ const RootLayout = async ({ children }: Readonly<{ children: React.ReactNode }>)
     <html lang="no">
       <head>
         <title>Din uføretrygd - nav.no</title>
+        <NaisMetaTags />
         <Decorator.HeadAssets />
       </head>
       <body>
         <Theme>
-          <div className="layout-wrapper">
-            <Decorator.Header />
-            <RepresentasjonBanner />
-            <main className="main-content" id="maincontent" tabIndex={-1}>
-              <Brødsmulesti mode="borger" />
-              {children}
-            </main>
-            <Decorator.Footer />
-            <Decorator.Scripts loader={Script} />
-            <script type="module" src={`${REPRESENTASJON_BANNER}/banner.js`} async></script>
-            <script src="https://widget.uxsignals.com/embed.js" async></script>
-            <FullmaktModal />
-            <InitializeFaro url={env('FARO_URL')} appName={env('NAIS_APP_NAME')} />
-          </div>
+          <ApmErrorBoundary fallback={<p>Noe gikk galt.</p>}>
+            <div className="layout-wrapper">
+              <Decorator.Header />
+              <RepresentasjonBanner />
+              <main className="main-content" id="maincontent" tabIndex={-1}>
+                <Brødsmulesti mode="borger" />
+                {children}
+                <Suspense fallback={null}>
+                  <ApmRouteTracker />
+                </Suspense>
+              </main>
+              <Decorator.Footer />
+              <Decorator.Scripts loader={Script} />
+              <script type="module" src={`${REPRESENTASJON_BANNER}/banner.js`} async></script>
+              <script src="https://widget.uxsignals.com/embed.js" async></script>
+              <FullmaktModal />
+            </div>
+          </ApmErrorBoundary>
         </Theme>
       </body>
     </html>
