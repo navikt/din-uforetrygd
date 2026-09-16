7160760321753bf2d558e299dcfcc32916157742
ludviklund <lulu@computas.com>
2026-09-16 14:57:11 +0200

    UFO-748: Fikset miljøvariabelnavn til å matche det vi bruker i actions

--- din-uforetrygd-frontend/next.config.mjs
+++ din-uforetrygd-frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/uforetrygd/selvbetjening',
-  // Nødvendig for at Nais APM skal kunne deobfuskere stack traces server-side.
+  // Nødvendig for at Nais APM skal kunne deobfuskere stack trace, se https://docs.nais.io/observability/frontend/how-to/sourcemaps/
   productionBrowserSourceMaps: true,
 }
 
